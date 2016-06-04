0e4edda0093c6e77254562e3dad56562702c7ae2
Randy Bastian <randy.bastbast@gmail.com>
2016-06-04 08:03:54 +0700

    First upload

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer id="footer">
+		<div class="footer-top">
+			<div class="container text-center">
+				<div class="logo-icon"><img class="img-responsive" src="images/presets/preset1/logo-icon.png" alt="" /></div>
+			</div>
+		</div>
+		<div class="footer-menu">
+			<div class="container">
+				<ul class="nav navbar-nav">                       
+					<li><a href="#">Apartment</a></li>
+					<li><a href="#">Decoration</a></li>
+					<li><a href="#">Design</a></li>
+					<li><a href="#">Development</a></li>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Land</a></li>
+				</ul> 
+			</div>
+		</div>
+		<div class="footer-bottom">
+			<div class="container text-center">
+				<p><a href="#">Portal Estates </a>&copy; 2016 </p>
+			</div>
+		</div>		
+	</footer>		
+	<!--/#scripts--> 
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.min.js"></script> 
+	<script type="text/javascript" src="js/moment.min.js"></script> 
+	<script type="text/javascript" src="js/jquery.simpleWeather.min.js"></script> 
+	<script type="text/javascript" src="js/jquery.sticky-kit.min.js"></script>
+	<script type="text/javascript" src="js/jquery.easy-ticker.min.js"></script> 
+	<script type="text/javascript" src="js/jquery.subscribe-better.min.js"></script> 
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
